a38ea738bf4abeb4f3407d27a1fc70fed60c447a
fluxworks <mrjustinlucas@gmail.com>
2026-06-02 16:16:42 -0400

    26.06.02.1615

---
+++ src/term/mod.rs
@@ -0,0 +1,28 @@
+/*!
+*/
+pub mod a; pub use self::a::{ * };
+pub mod b; pub use self::b::{ * };
+pub mod c; pub use self::c::{ * };
+pub mod d; pub use self::d::{ * };
+pub mod e; pub use self::e::{ * };
+pub mod f; pub use self::f::{ * };
+pub mod g; pub use self::g::{ * };
+pub mod h; pub use self::h::{ * };
+pub mod i; pub use self::i::{ * };
+pub mod j; pub use self::j::{ * };
+pub mod k; pub use self::k::{ * };
+pub mod l; pub use self::l::{ * };
+pub mod m; pub use self::m::{ * };
+pub mod n; pub use self::n::{ * };
+pub mod o; pub use self::o::{ * };
+pub mod p; pub use self::p::{ * };
+pub mod q; pub use self::q::{ * };
+pub mod r; pub use self::r::{ * };
+pub mod s; pub use self::s::{ * };
+pub mod t; pub use self::t::{ * };
+pub mod u; pub use self::u::{ * };
+pub mod v; pub use self::v::{ * };
+pub mod w; pub use self::w::{ * };
+pub mod x; pub use self::x::{ * };
+pub mod y; pub use self::y::{ * };
+pub mod z; pub use self::z::{ * };
